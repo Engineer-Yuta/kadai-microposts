b8ace621f7f1629262d0b64c1da74fc219cf8102
engineer.y.o <engineer.y.o3911@gmail.com>
2020-01-30 10:38:30 +0000

    favolite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_many :reverses_of_favolites, class_name: "Favorite", foreign_key: "micropost_id"
+  has_many :favorite_users , through: :reverses_of_favolites, source: :user
+  has_many :reverses_of_favolites, class_name: "Favorite", dependent: :destroy
   
   validates :content, presence: true, length: {maximum: 255}
 end
